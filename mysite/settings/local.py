bcef17c6dfcb849fdebf97f49f83b9f4629e3248
henrydavidzhu <henry.david.zhu@gmail.com>
2016-04-21 00:16:15 -0700

    asdpfoih

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -120,8 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '../webapp/static/'
 
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'webapp/static'),
+)
 
 LOGIN_URL = '/login/'
