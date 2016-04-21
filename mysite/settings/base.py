0f23656be34682119e8d7980a8071cd3dc3d56f7
henrydavidzhu <henry.david.zhu@gmail.com>
2016-04-20 23:27:27 -0700

    asdfoihofih

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
