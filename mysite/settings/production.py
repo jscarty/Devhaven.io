531d7be4e7dd7d2a277f7b814d33cf2a4a937466
henrydavidzhu <henry.david.zhu@gmail.com>
2016-04-21 20:19:15 -0700

    asdf

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 DEBUG = False
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 DATABASES = settings.DATABASES
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
