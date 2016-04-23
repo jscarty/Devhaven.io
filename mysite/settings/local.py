1e37eea979e88f62ea8aec938322b60334bf3291
henrydavidzhu <henry.david.zhu@gmail.com>
2016-04-23 15:24:38 -0700

    asdf

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -72,6 +72,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
