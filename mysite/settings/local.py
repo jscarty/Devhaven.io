c9807e963a9a84b7c03b8030baac08634190c172
henrydavidzhu <henry.david.zhu@gmail.com>
2016-04-23 14:27:37 -0700

    asdf

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -73,16 +73,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbpt1duqgntocd',                     
-        'USER': 'lpdzvtukzvnfla',
-        'PASSWORD': 'oOjfBzWzF0cjGyGglZjZX_m7tx',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'dbpt1duqgntocd',                     
+#        'USER': 'lpdzvtukzvnfla',
+#        'PASSWORD': 'oOjfBzWzF0cjGyGglZjZX_m7tx',
+#        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+#        'PORT': '5432',                      # Set to empty string for default.
+#    }
+#}
 
 
 
