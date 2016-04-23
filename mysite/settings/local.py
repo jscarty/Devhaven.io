06e71bf56dc1fa04dcdc6d6b8f999e4df751e998
henrydavidzhu <henry.david.zhu@gmail.com>
2016-04-23 16:07:52 -0700

    asdf

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -73,8 +73,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 
 # Password validation
