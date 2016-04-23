5a6f67686784330f81acf6e09e5b2dcbf2122306
henrydavidzhu <henry.david.zhu@gmail.com>
2016-04-23 10:11:24 -0700

    'asdf

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',                     
+        'USER': 'myuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
-
 
 
 # Password validation
