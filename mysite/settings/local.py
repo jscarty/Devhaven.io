cdd5f723c5ac2ce51c0eab108688a322ac3795e9
henrydavidzhu <henry.david.zhu@gmail.com>
2016-04-22 19:09:45 -0700

    asdf

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -73,15 +73,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb', 
+        'USER': 'myuser',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
