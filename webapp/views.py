916d6a21c1a2c7a3bb673a3bb740710617d34b43
henrydavidzhu <henry.david.zhu@gmail.com>
2016-05-20 18:38:36 -0700

    asdf

--- webapp/views.py
+++ webapp/views.py
@@ -205,7 +205,7 @@ def post_list(request):
     return render(request, 'webapp/threadfeed.html', context)
 
 def donate(request):
-    return render(request, 'webapp/donate.html')
+    return render(request, 'webapp/donate.html', {'authenticated': request.user.is_authenticated()})
 
 def your_post(request):
     posts2 = Post.objects.filter(author__username=str(request.user))
