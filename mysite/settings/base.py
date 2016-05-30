b3181409a08cc61432589226d936ab0867031f5f
henrydavidzhu <henry.david.zhu@gmail.com>
2016-05-29 22:30:27 -0700

    asdf

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 SECRET_KEY = '4(%6tae$hut71^0us2z)fbs1bc&a+tjw76yi+f0-k-fxbu5#(i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
